2a5f5261ab51e00746a47a85a15833143cba44c1
gupta-anil <ganil1307@gmail.com>
2016-11-30 12:14:37 +0530

    Added Binary Search Algorithm

---
+++ code/BinSearch.cc
@@ -0,0 +1,15 @@
+#include "template.h"
+bool works(int m){
+  // if Array[m] is the value to be searched, return true
+  // else return false
+}
+
+// The property is increasing
+int BinarySearch(int l, int h){
+  int m;
+  while(l <= h){ m = (l+h) / 2; if(works(m))  l=m+1; else h=m-1; }
+  return l-1;
+}
+// /*The property is decreasing
+// while(l <= h){ m = (l+h) / 2; if(works(m))  h=m-1; else l=m+1; }
+// return h+1; /*
