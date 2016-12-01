b5f08a10816da764190ed068efc168dbaf03b486
gupta-anil <ganil1307@gmail.com>
2016-12-01 09:26:12 +0530

    Added 2-D BIT

---
+++ code/BIT2D.cc
@@ -0,0 +1,19 @@
+/* Bit used as 2-D structure for a 2-D plane listing the points in rectangle  */
+#include "template.h"
+int bit[M][M], n;
+int sum( int x, int y ){
+    int ret = 0;
+    while( x > 0 ){
+        int yy = y; while( yy > 0 ) ret += bit[x][yy], yy -= yy & -yy;
+        x -= (x & -x);
+    }
+    return ret ;
+}
+void update(int x , int y , int val){
+    int y1;
+    while (x <= n){
+        y1 = y;
+        while (y1 <= n){ bit[x][y1] += val; y1 += (y1 & -y1); }
+        x += (x & -x); 
+    }
+}
