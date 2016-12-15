52fdf78e3680dfa12ab0fe0ee5c0271dc6606d5b
Ayush Tulsyan <ayushtuls.64@gmail.com>
2016-12-15 17:45:51 +0530

    Hopcraft Karp and Minor changes

--- code/template.h
+++ code/template.h
@@ -8,6 +8,7 @@ const double eps = 1e-10;
 
 typedef long long ll;
 typedef vector <int> vi;
+typedef vector <vi> vvi;
 typedef vector <ll> vl;
 typedef pair <int, int> pii;
 typedef pair <ll, ll> pll;
@@ -23,5 +24,5 @@ typedef vector <pll> vll;
 #define rep2(i,b,a) for(ll i = (ll)b, _a = (ll)a; i >= _a; i--)
 #define rep1(i,a,b) for(ll i = (ll)a, _b = (ll)b; i <= _b; i++)
 #define rep(i,n) for(ll i = 0, _n = (ll)n ; i < _n ; i++)
-#define mem(a,val) memset(a,val,sizeof(a))
+#define all(v) v.begin(), v.end()
 #define fast ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
